5047c9e8bf1a92de360eff9b59986c8985b65fcf
sijuz <sijuz@yandex.ru>
2023-08-15 12:41:01 +0300

    s

--- src/logic/coupon.ts
+++ src/logic/coupon.ts
@@ -2,7 +2,7 @@
 import { DeLabConnect, DeLabTransaction } from '@delab-team/connect'
 // import { Cell, toNano } from 'ton-core'
 
-import { Address, beginCell, Cell, contractAddress, StateInit, storeStateInit, toNano } from 'ton'
+import { Address, beginCell, Cell, contractAddress, StateInit, storeStateInit, toNano, TonClient } from 'ton'
 import { KeyPair, keyPairFromSeed, sha256, sign } from 'ton-crypto'
 import { StorageWallet } from './storage'
 import { ClaimFunctions, OneTimeCheque, Opcodes } from './wrappers/OneTimeCheque'
@@ -12,8 +12,12 @@ const oneBoc = 'b5ee9c72410106010055000114ff00f4a413f4bcf2c80b010201200302004ef2
 export class Coupon {
     private _wallet: DeLabConnect
 
+    private _client: TonClient
+
     constructor (wallet: DeLabConnect) {
         this._wallet = wallet
+
+        this._client = new TonClient({ endpoint: 'https://testnet.tonhubapi.com/jsonRPC' })
     }
 
     public async deployOne (passwordString: string, amount: string | number): Promise<boolean> {
@@ -90,16 +94,26 @@ export class Coupon {
 
         console.log('oneTimeCheque', oneTimeCheque)
 
-        const payload = beginCell().storeUint(Opcodes.claim, 32).storeBuffer(signature).storeAddress(addressFor)
-            .endCell()
-        const trans: DeLabTransaction = {
-            to: Address.parse(address).toString({ bounceable: true }),
-            value: toNano('0.05').toString(),
-            payload: payload.toBoc().toString('base64')
-        }
+
+        const oneTimeChequeO = this._client.open(
+            OneTimeCheque.createFromAddress(Address.parse(address))
+        )
+
+        const tx = await oneTimeChequeO.sendClaim({
+            signature,
+            address: addressFor
+        })
+
+        // const payload = beginCell().storeUint(Opcodes.claim, 32).storeBuffer(signature).storeAddress(addressFor)
+        //     .endCell()
+        // const trans: DeLabTransaction = {
+        //     to: Address.parse(address).toString({ bounceable: true }),
+        //     value: toNano('0.05').toString(),
+        //     payload: payload.toBoc().toString('base64')
+        // }
 
         try {
-            const tx = await this._wallet.sendTransaction(trans)
+            // const tx = await this._wallet.sendTransaction(trans)
             console.log('tx', tx)
             return true
         } catch (error) {
