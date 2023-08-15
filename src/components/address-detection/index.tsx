2bc7c716d1f3d101eb15ed599ecd80ca57a1d49c
anovic123 <105565287+anovic123@users.noreply.github.com>
2023-08-16 00:46:57 +0300

    commit

--- src/components/address-detection/index.tsx
+++ src/components/address-detection/index.tsx
@@ -7,16 +7,14 @@ interface AddressDetectionProps {
 export const AddressDetection: FC<AddressDetectionProps> = ({ text }) => {
     const detectAddress = (text: string) => {
         const addressRegex = (
-          //
+
         )
 
-        // return matches ? matches[0] : 'Address not found'
     }
 
-    const detectedAddress = detectAddress(text)
     return (
         <div>
-            <p>{detectedAddress}</p>
+            <p></p>
         </div>
     )
 }
