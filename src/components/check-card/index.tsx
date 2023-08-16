75a3aa8d788f8b24c7f9033b0b5cbcd02b1efa56
anovic123 <105565287+anovic123@users.noreply.github.com>
2023-08-16 20:48:44 +0300

    fixed check-card styles

--- src/components/check-card/index.tsx
+++ src/components/check-card/index.tsx
@@ -62,7 +62,7 @@ export const CheckCard: FC<CheckCardProps> = ({ el, index, handleCheckCardClick 
                             : el.typeCheck === 'Multicheck'
                                 ? 'Multicheck'
                                 : null}
-                        <span className={s.checkIndex}># {index}</span>
+                        <span className={s.checkIndex}>#{index}</span>
                     </p>
                     <p className={s.checkSum}>
                         Sum: {fixAmount(bal)} TON (
