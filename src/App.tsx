c8babd0d94011aef6c5164f5b0341928f34cc414
sijuz <sijuz@yandex.ru>
2023-08-19 20:03:02 +0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import { useEffect, useState } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import { Address, TonClient } from 'ton'
 import {
+    DeLabModal,
     DeLabConnect,
+    DeLabNetwork,
+    DeLabTypeConnect,
+    DeLabAddress,
     DeLabConnecting,
     DeLabEvent
 } from '@delab-team/connect'
 
-import { useTonAddress } from '@tonconnect/ui-react'
+import { TonConnectUIProvider, useTonAddress } from '@tonconnect/ui-react'
+import { Locales, useTonConnectUI } from '@tonconnect/ui-react'
 
 import { ToastContainer } from 'react-toastify'
 
@@ -27,48 +32,96 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const isTestnet = window.location.host.indexOf('localhost') >= 0 ? true : window.location.href.indexOf('testnet') >= 0
 
-const DeLabConnector = new DeLabConnect('https://delabteam.com/', 'DeCoupons', isTestnet ? 'testnet' : 'mainnet')
-
 export const App = (): JSX.Element => {
     const [ firstRender, setFirstRender ] = useState<boolean>(false)
 
-    const [ tonClient, setTonClient ] = useState<TonClient>(new TonClient({ endpoint: isTestnet ? 'https://testnet.tonhubapi.com/jsonRPC' : 'https://mainnet.tonhubapi.com/jsonRPC' }))
-
     const [ isConnected, setIsConnected ] = useState<boolean>(false)
+
+    const [ address, setAddress ] = useState<DeLabAddress>(undefined)
+    const [ network, setNetwork ] = useState<DeLabNetwork>('testnet')
     const [ balance, setBalance ] = useState<string | undefined>(undefined)
+    const [ typeConnect, setTypeConnect ] = useState<DeLabTypeConnect>(undefined)
+
+    const [ tonClient, setTonClient ] = useState<TonClient>(new TonClient({ endpoint: isTestnet ? 'https://testnet.tonhubapi.com/jsonRPC' : 'https://mainnet.tonhubapi.com/jsonRPC' }))
 
     const [ addressCoupon, setAddressCoupon ] = useState<string>('')
 
-    const rawAddress = useTonAddress(false)
+    // =================================
+
+    const RawAddress = useTonAddress()
+
+    // ==================================
 
     function listenDeLab () {
         DeLabConnector.on('connect', async (data: DeLabEvent) => {
             setIsConnected(true)
             const connectConfig: DeLabConnecting = data.data
+            setAddress(connectConfig.address)
+            setTypeConnect(connectConfig.typeConnect)
+            setNetwork(connectConfig.network)
+
             if (connectConfig.address) {
                 const bl = await tonClient.getBalance(Address.parse(connectConfig.address))
 
                 setBalance(bl.toString())
             }
         })
+        DeLabConnector.on('disconnect', () => {
+            setIsConnected(false)
+            setAddress(undefined)
+            setTypeConnect(undefined)
+            setNetwork('testnet')
+            console.log('disconnect')
+        })
+
+        DeLabConnector.on('error', (data: DeLabEvent) => {
+            console.log('error', data.data)
+        })
+
+        DeLabConnector.on('error-transaction', (data: DeLabEvent) => {
+            console.log('error-transaction', data.data)
+        })
+
+        DeLabConnector.on('error-toncoinwallet', (data: DeLabEvent) => {
+            console.log('error-toncoinwallet', data.data)
+        })
+
+        DeLabConnector.on('error-tonhub', (data: DeLabEvent) => {
+            console.log('error-tonhub', data.data)
+        })
+
+        DeLabConnector.on('error-tonkeeper', (data: DeLabEvent) => {
+            console.log('error-tonkeeper', data.data)
+        })
+
+        DeLabConnector.loadWallet()
     }
 
     useEffect(() => {
-        if (!firstRender && DeLabConnector) {
+        if (!firstRender) {
             setFirstRender(true)
             listenDeLab()
         }
     }, [])
 
+    useEffect(() => {
+
+    }, [ ])
+
     const navigate = useNavigate()
 
+    // ===============================
+
     useEffect(() => {
-        if (!rawAddress) {
+        if (!RawAddress) {
             navigate(ROUTES.LOGIN)
         } else {
+            tonClient.getBalance(Address.parse(RawAddress)).then((bl) => {
+                setBalance(bl.toString())
+            })
             navigate(ROUTES.YOUR_CHECKS)
         }
-    }, [ rawAddress ])
+    }, [ RawAddress ])
 
     return (
         <>
@@ -83,6 +136,8 @@ export const App = (): JSX.Element => {
                         element={
                             <CreateCheckPage
                                 balance={balance}
+                                address={address}
+                                typeConnect={typeConnect}
                             />
                         }
                     />
@@ -99,6 +154,9 @@ export const App = (): JSX.Element => {
                         path={ROUTES.SETTINGS}
                         element={
                             <SettingsPage
+                                isConnected={isConnected}
+                                address={address}
+                                balance={balance}
                                 isTestnet={isTestnet}
                             />
                         }
@@ -108,15 +166,16 @@ export const App = (): JSX.Element => {
                         path={ROUTES.ACTIVATE}
                         element={
                             <Activate
-                                setAddress={setAddressCoupon}
+                                balance={balance}
                                 address={addressCoupon}
+                                addressWallet={address}
                             />
                         }
                     />
                 </Route>
                 <Route
                     path={ROUTES.LOGIN}
-                    element={<LoginPage />}
+                    element={<LoginPage  />}
                 />
             </Routes>
             <ToastContainer
